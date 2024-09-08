65f71afead48589d47687830265555f42ff8e7cc
Abhishek Kumar <123209848+Abhi-Ary@users.noreply.github.com>
2024-09-08 18:17:31 +0000

    datatypes

---
+++ codes/datatypes.js
@@ -0,0 +1,26 @@
+"use strict"; //treat all js code as newer version
+// alert(3 + 3) it gives an error bcz we are using nodejs, not browser
+console.log(3 + 3);
+console.log("Hitesh");
+// code redability should be high
+
+let name = "Abhishek"
+let age = 18
+let isLoggedIn = false
+let state;
+
+
+// datatypes
+// number => 2 to power of 53
+// bigInt
+// string => "" / ''
+// boolean => true/false
+// null => standalone value or its a representation of empty value
+// undefind
+// symbol => unique
+
+// object
+
+console.log(typeof null);//object
+console.log(typeof undefined);//undefind
+
