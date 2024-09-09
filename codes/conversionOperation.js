fcecbe48f13bdb0bdc1bfb3986bb7d8125fa77a1
Abhishek Kumar <123209848+Abhi-Ary@users.noreply.github.com>
2024-09-09 11:50:34 +0000

    Operations on datatypes

--- codes/conversionOperation.js
+++ codes/conversionOperation.js
@@ -22,5 +22,65 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-console.log(typeof stringNumber);
+// console.log(typeof stringNumber);
+
+// ******************************** Operations ***********************************
+let value = 3
+let negValue = -value
+// console.log(negValue);
+// console.log(2+2);//4
+// console.log(2-2);//0
+// console.log(2*2);//4
+// console.log(2**2);//4
+// console.log(2**3);//8
+// console.log(2/3);//0.6666666666666666
+// console.log(2%3);//2
+
+let str1 = "Hello"
+let str2 = " Abhishek"
+let str3 = str1 + str2
+// console.log(str3); Hello Abhishek
+
+// console.log("1" + 2);//12
+// console.log(1 + "2");//12
+// console.log("1" + 2 + 2);//122
+// console.log(1 + 2 + "2");//32
+// console.log((((3 + 4 )* 5) % 3)); //2
+// console.log(true);//true
+// console.log(+true);// 1
+// console.log(+""); // 0
+// console.log(-true); // -1
+// console.log(-""); // -0
+
+let num1, num2, num3
+num1 = num2 = num3 = 2 + 2
+// console.log(num1, num2, num3); // 4 4 4
+
+let gameCounter = 100
+// gameCounter++
+// console.log(gameCounter);// 101
+++gameCounter
+// console.log(gameCounter);// 101
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
