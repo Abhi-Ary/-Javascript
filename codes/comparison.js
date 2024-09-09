89284eaf2f99e8eda7e170539b12d3a4a00655cc
Abhishek Kumar <123209848+Abhi-Ary@users.noreply.github.com>
2024-09-09 12:12:49 +0000

    comparison of datatypes

---
+++ codes/comparison.js
@@ -0,0 +1,34 @@
+// console.log(2 > 1); true
+// console.log(2 >= 1); true
+// console.log(2 < 1); false
+// console.log(2 <= 1); false
+// console.log(2 == 1); false
+// console.log(2 != 1); true
+
+// console.log("2" > 1); true bcz js by default converted string value into number
+// console.log("02" > 1); true bcz js by default converted string value into number
+
+// console.log(null > 0); false
+// console.log(null >= 0); true bcz an equality check == and comparisons <,>, <=, >= work differently comparison convert the null into a number treating as 0
+// console.log(null == 0); false
+
+// console.log(undefined == 0); false
+// console.log(undefined > 0); false
+// console.log(undefined < 0); false
+
+// === it checks values as well as data types
+// console.log("2" === 2); false
+
+
+
+
+
+
+
+
+
+
+
+
+
+
