42bd80b9499999d35cf0a7daddf19d363381310e
Abhishek Kumar <123209848+Abhi-Ary@users.noreply.github.com>
2024-09-10 16:13:45 +0000

    dates

---
+++ codes/dates.js
@@ -0,0 +1,42 @@
+let myDate = new Date()
+// console.log(myDate);
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toLocaleDateString());
+// console.log(myDate.toLocaleString());
+// console.log(myDate.toTimeString());
+// console.log(typeof myDate);
+
+// let myCreatedDate = new Date(2024, 8, 10)
+// console.log(myCreatedDate.toDateString());
+
+// let myCreatedDate = new Date("2024-09-10")
+let myCreatedDate = new Date("10-09-2024")
+// console.log(myCreatedDate.toLocaleString());
+
+let myTimeStamp =  Date.now()
+// console.log(myTimeStamp);
+
+// console.log(Math.floor(Date.now() / 1000));
+
+let newDate = new Date()
+console.log(newDate);
+// console.log(newDate.getMonth());
+// console.log(newDate.getDay());
+
+newDate.toLocaleString('default', {
+    weekday: "long"
+})
+
+
+
+
+
+
+
+
+
+
+
+
