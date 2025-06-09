89ac59b7c3363daf49e184a76aa1b0afe418bbee
Abhishek Kumar <123209848+Abhi-Ary@users.noreply.github.com>
2025-06-09 08:41:24 +0000

    variables updated

--- codes/variables.js
+++ codes/variables.js
@@ -1,9 +1,10 @@
-const accountId = 1349549
-let accountEmail = "abhshek@google.com"
+const accountId = 1349549 // When i make sure that the value of variable can't chage in future then it will be declared by the const keyword
+let accountEmail = "abhshek@google.com" // But when i make sure that the value of a variable will be change in future then it will be declare by the let keyword
 var accountPassword = "4848y8u"
-accountCity = "Dehradun"
+accountCity = "Dehradun" // not recommended
 let accountState;
 
+// accountId = 234 we can't do this
 accountCity = "Premnagar" // we can change this also
 accountPassword = "3848" // we can change the value of var as well
 accountEmail = "ap@ap.com" // we can change the value of let
@@ -11,6 +12,6 @@ accountEmail = "ap@ap.com" // we can change the value of let
 console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
 /*
 prefer not to use the var
-because of issues in block of scope and functional scope
+bcz of issues in block of scope and functional scope
 In javascript if we declare a variable but not assing a value to it then it gives undefind
 */
