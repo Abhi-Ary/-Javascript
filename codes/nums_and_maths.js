5e33f9f98d3cc061e6574b99a6ce412cff3cade9
Abhishek Kumar <123209848+Abhi-Ary@users.noreply.github.com>
2025-06-10 08:59:45 +0000

    nums updated

--- codes/nums_and_maths.js
+++ codes/nums_and_maths.js
@@ -1,7 +1,7 @@
 const score = 400
 // console.log(score);
 
-const balance = new Number(400)
+const balance = new Number(400) // It specifies only numbers
 // console.log(balance);
 
 // console.log(balance.toString().length);
